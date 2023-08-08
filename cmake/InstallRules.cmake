0ac11146b81c9f78017a4df16946c8ecf980b9f9
Andrew Kaster <akaster@serenityos.org>
2023-08-07 23:20:47 -0600

    Meta: Build with a two-stage build where we install Lagom first

--- cmake/InstallRules.cmake
+++ cmake/InstallRules.cmake
@@ -1,17 +1,9 @@
 include(CMakePackageConfigHelpers)
 include(GNUInstallDirs)
 
-set(package ladybird)
+set(package ladybird-gtk4)
 
-install(TARGETS LibAudio LibCore LibFileSystem LibGfx LibIPC LibJS LibWeb LibWebView LibWebSocket LibProtocol LibGUI LibMarkdown LibGemini LibHTTP LibGL LibSoftGPU LibVideo LibWasm LibXML LibIDL LibTextCodec LibCrypto LibLocale LibRegex LibSyntax LibUnicode LibCompress LibTLS LibGLSL LibGPU LibThreading LibSQL
-  EXPORT ladybirdTargets
-  LIBRARY
-    COMPONENT ladybird_Runtime
-    NAMELINK_COMPONENT ladybird_Development
-    DESTINATION ${CMAKE_INSTALL_LIBDIR}
-)
-
-install(TARGETS ladybird WebContent
+install(TARGETS ladybird
   EXPORT ladybirdTargets
   RUNTIME
     COMPONENT ladybird_Runtime
@@ -49,27 +41,7 @@ install(
 
 install(
   EXPORT ladybirdTargets
-  NAMESPACE ladybird::
+  NAMESPACE ladybird-gtk4::
   DESTINATION "${ladybird_INSTALL_CMAKEDIR}"
   COMPONENT ladybird_Development
-)
-
-install(DIRECTORY
-    "${SERENITY_SOURCE_DIR}/Base/res/html"
-    "${SERENITY_SOURCE_DIR}/Base/res/fonts"
-    "${SERENITY_SOURCE_DIR}/Base/res/icons"
-    "${SERENITY_SOURCE_DIR}/Base/res/themes"
-    "${SERENITY_SOURCE_DIR}/Base/res/color-palettes"
-    "${SERENITY_SOURCE_DIR}/Base/res/cursor-themes"
-  DESTINATION "${CMAKE_INSTALL_DATADIR}/res"
-  USE_SOURCE_PERMISSIONS MESSAGE_NEVER
-  COMPONENT ladybird_Runtime
-)
-
-install(FILES
-    "${SERENITY_SOURCE_DIR}/Base/home/anon/.config/BrowserAutoplayAllowlist.txt"
-    "${SERENITY_SOURCE_DIR}/Base/home/anon/.config/BrowserContentFilters.txt"
-    "${Lagom_BINARY_DIR}/cacert.pem"
-  DESTINATION "${CMAKE_INSTALL_DATADIR}/res/ladybird"
-  COMPONENT ladybird_Runtime
 )
